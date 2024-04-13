a99778ba390ec235ce3f9eac3ef73e14ab963082
OlhaMajerski <olha1991o@gmail.com>
2024-04-13 10:34:02 +0200

    edit code

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,8 @@
 function formatMessage(message, maxLength) {
-    if (message <= maxLength) {
+    if (message.length <= maxLength) {
         return message;
     } else {
-        const truncatedMessage = message.slice(0, maxLength) + "..."
-        return truncatedMessage;
+        return message.slice(0, maxLength) + "...";
     }
 }
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function checkForSpam(message) {
-    const lowerCaseMessage = message.toLowerCase;
+    const lowerCaseMessage = message.toLowerCase();
     if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
         return true;
     } else {
